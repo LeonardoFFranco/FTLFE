3ad9f26ced6fb36c14aee86eec4db98c0b0d38a8
LeonardoFFranco <fabrizio.07.ff@gmail.com>
2018-06-08 09:12:47 -0400

    Update playlists.service.ts

--- src/app/playlists/playlists.service.ts
+++ src/app/playlists/playlists.service.ts
@@ -8,22 +8,7 @@ export class PlaylistsService {
 
   constructor(
     private http: Http) {}
-
-  crearPlaylist(id, newList) {
-    let url = `http://localhost:8000/FlameTuneLibrary/playlist/create/${id}`;
-    let data = JSON.stringify(newList);
-    let headers = new Headers();
-    headers.append('Content-Type', 'application/json');
-    headers.append('Accept', 'application/json');
-    let requestOptions = new RequestOptions({
-      method: RequestMethod.Post,
-      url: url,
-      headers: headers,
-      body: data
-    });
-    return this.http.post(url,data,requestOptions).forEach(res => res.json());
-  }
-
+  
   updatePlaylist(id, newList) {
     let url = `http://localhost:8000/FlameTuneLibrary/playlist/update/${id}`;
     let data = JSON.stringify(newList);
@@ -38,5 +23,32 @@ export class PlaylistsService {
     });
     return this.http.put(url,data,requestOptions).forEach(res => res.json());
   }
-
+crearPlaylist(id, newList) {
+    let url = `http://localhost:8000/FlameTuneLibrary/playlist/create/${id}`;
+    let data = JSON.stringify(newList);
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    headers.append('Accept', 'application/json');
+    let requestOptions = new RequestOptions({
+      method: RequestMethod.Post,
+      url: url,
+      headers: headers,
+      body: data
+    });
+    return this.http.post(url,data,requestOptions).forEach(res => res.json());
+  }
+  borrarPlaylist(id) {
+    let url = `http://localhost:8000/FlameTuneLibrary/playlist/delete/${id}`;
+  //  let data = JSON.stringify(newList);
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    headers.append('Accept', 'application/json');
+    let requestOptions = new RequestOptions({
+      method: RequestMethod.Delete,
+      url: url,
+      headers: headers,
+    //  body: data
+    });
+    return this.http.delete(url,requestOptions).forEach(res => res.json());
+  }
 }
