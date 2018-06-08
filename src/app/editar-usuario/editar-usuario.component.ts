0c97f26d4b3006bf5c7dea7e767fb6eda76ecf6e
LeonardoFFranco <fabrizio.07.ff@gmail.com>
2018-06-08 03:01:54 -0400

    Update editar-usuario.component.ts

--- src/app/editar-usuario/editar-usuario.component.ts
+++ src/app/editar-usuario/editar-usuario.component.ts
@@ -15,6 +15,8 @@ export class EditarUsuarioComponent implements OnInit {
   data_cantidad_membresias:any;
   data_fecha_inicio_membresia:any;
   data_numero_tarjeta:any;
+  imageUrl: string = "";
+  fileToUpload: File = null;
   constructor(private serviciod : EditarUsuarioService) { }
   update(event, id) {
 
@@ -31,6 +33,15 @@ export class EditarUsuarioComponent implements OnInit {
     }
     this.serviciod.editarUsuario(this.data_id, newUsuario);
     //this.cargar()
+  }
+
+  handleFileInput(file: FileList) {
+    this.fileToUpload = file.item(0);
+    var reader = new FileReader();
+    reader.onload = (event:any) => {
+      this.imageUrl = event.target.result;
+    }
+    reader.readAsDataURL(this.fileToUpload);
   }
   ngOnInit() {
   }
